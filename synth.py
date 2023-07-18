c14916e51a926e145a35b63bca2f6ae69ac17664
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2023-07-18 15:23:38 +0200

    upgrade chirp texture synth

--- synth.py
+++ synth.py
@@ -3,38 +3,57 @@ from scipy.signal.windows import tukey
 import torch
 
 # duration = 4
+# event_duration = 2**(-4)
 # sr = 2**13
 # fmin = 2**8
-# fmax = 2**9
-# n_events = 128
-# threshold_db = -10
+# fmax = 2**11
+# Q = 24
+# hop_length = 2**6
+# n_events = 2**6
+
+def get_amplitudes(theta_density, n_events):
+    offset = 0.25 * theta_density + 0.75 * theta_density**2
+    event_ids = torch.tensor(np.arange(n_events)).type(torch.float32)
+    sigmoid_operand = (1-theta_density) * n_events * (event_ids/n_events - offset)
+    amplitudes = 1 - torch.sigmoid(2*sigmoid_operand)
+    amplitudes = amplitudes / torch.max(amplitudes)
+    return amplitudes
+
+def get_slope(theta_slope, Q, hop_length, sr):
+    """theta_slope --> ±1 correspond to a near-vertical line.
+    theta_slope = 0 corresponds to a horizontal line.
+    The output is measured in octaves per second."""
+    typical_slope = sr / (Q * hop_length)
+    return torch.tan(theta_slope * np.pi/2) * typical_slope/8
 
 def generate_chirp_texture(
-        gamma,
-        log2_density,
-        *,
-        duration,
-        event_duration,
-        sr,
-        fmin,
-        fmax, 
-        n_events,
-        threshold_db):
-
-    # Enable automatic differentiation for gamma and log2_density
-    gamma = torch.tensor(gamma, requires_grad=True)
-    log2_density = torch.tensor(log2_density, requires_grad=True)
+    theta_density,
+    theta_slope,
+    *,
+    duration,
+    event_duration,
+    sr,
+    fmin,
+    fmax, 
+    n_events,
+    Q,
+    hop_length     
+):
+    # Define constant log(2)
+    const_log2 = torch.log(torch.tensor(2.0))
 
     # Define the amplitudes of the events
-    event_ids = torch.tensor(np.arange(n_events)).type(torch.int64)
-    density = torch.pow(2.0, log2_density)
-    amplitudes_db = event_ids * threshold_db / density
-    amplitudes = torch.nn.functional.softmax(amplitudes_db, dim=-1)
+    amplitudes = get_amplitudes(theta_density, n_events)
+
+    # Define the slope of the events
+    gamma = get_slope(theta_slope, Q, hop_length, sr)
 
     # Draw onsets at random
-    event_length = torch.tensor(event_duration * sr).type(torch.int64)
-    onsets = torch.floor(torch.rand(n_events) * (duration*sr - event_length))
-    onsets = onsets.type(torch.int64)
+    chirp_duration = 2 * event_duration / (torch.abs(theta_slope) + 0.25)
+    chirp_length = torch.tensor(chirp_duration * sr).type(torch.int64)
+    rand_onsets = torch.rand(n_events)
+    onsets = rand_onsets * (duration*sr/2-chirp_length) + duration * sr / 4
+    onsets = torch.floor(onsets).type(torch.int64)
 
     # Draw frequencies at random
     log2_fmin = torch.log2(torch.tensor(fmin))
@@ -44,20 +63,21 @@ def generate_chirp_texture(
 
     # Generate events one by one
     X = torch.zeros(duration*sr, n_events)
-    time = torch.arange(0, event_duration, 1/sr)
-    envelope = torch.tensor(tukey(event_length))
+    time = torch.arange(chirp_length)/sr - chirp_duration/2
+    envelope = torch.tensor(tukey(chirp_length))
+    event_ids = torch.arange(n_events)
     patch_zip = zip(event_ids, onsets, amplitudes, frequencies)
+
     for event_id, onset, amplitude, frequency in patch_zip:
-        chirp_phase = torch.expm1(gamma*torch.log(2)*time) / (gamma*torch.log(2))
-        chirp = torch.sin(frequency * chirp_phase)
-        offset = onset + event_length
+        if torch.abs(gamma) < 1e-6:
+            phase = time
+        else:
+            phase = torch.expm1(gamma*const_log2*time) / (gamma*const_log2)
+        chirp = torch.sin(2 * torch.pi * frequency * phase)
+        offset = onset + chirp_length
         X[onset:offset, event_id] = chirp * amplitude * envelope
 
     # Mix events
     x = X.sum(axis=-1)
 
-    # Apply a Tukey window onto the mixture
-    window = torch.tensor(tukey(duration*sr))
-    y = x * window
-
-    return y
+    return x
